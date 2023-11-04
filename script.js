9a584962ed49992fb248220245d66bd9fb3e4758
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2023-11-03 22:53:50 -0300

    :sparkles: feat: add logic to change photo according to theme

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("lighty")
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+  html.classList.contains("light")
+    ? img.setAttribute("src", "./assets/avatar-light.png")
+    : img.setAttribute("src", "./assets/avatar.png")
 }
